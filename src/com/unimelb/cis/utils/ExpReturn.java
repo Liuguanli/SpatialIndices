d7020b365355db99c5255ba1e2f46e28d4638433
Guanli Liu <liuguanli1205@qq.com>
2019-09-28 11:04:28 +1000

    add result list to ExpReturn

--- src/com/unimelb/cis/utils/ExpReturn.java
+++ src/com/unimelb/cis/utils/ExpReturn.java
@@ -1,10 +1,17 @@
 package com.unimelb.cis.utils;
 
+import com.unimelb.cis.node.Point;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExpReturn {
 
     public long time;
 
     public int pageaccess;
+
+    public List<Point> result = new ArrayList<>();
 
     @Override
     public String toString() {
